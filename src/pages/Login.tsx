8e703fcd2e1fa2bf8a2efe643361f729e439119c
Fahmi Ihsan <fahmi.ihsan@student.umn.ac.id>
2022-04-17 05:28:47 +0700

    Modify Login and SignUp Page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,23 +1,58 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCol, IonContent, IonGrid,  IonPage, IonRow, IonTitle,  IonText, IonButton} from '@ionic/react';
+import { useRef, useState } from 'react';
+import {Link} from 'react-router-dom';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Login: React.FC = () => {
+  const [isLogin, setIsLogin] = useState(false);
+  const btnLogin = useRef<HTMLIonButtonElement>(null);
+
+  const linkStyle = {
+    margin: "1rem",
+    color: 'white'
+  };
+
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
+        <IonContent color='primary'>
+          <IonGrid className='content-login-signup'>
+            <IonRow className="ion-text-center">
+              <IonCol className="ion-align-items-center">
+                <IonTitle className='app-title'>Waddup</IonTitle> 
+              </IonCol>
+            </IonRow>
+            <IonRow className='ion-justify-content-center'>
+              <IonCol className="ion-align-items-center">
+                <form action="submit" className='ion-margin login_form '>
+                  <label>Email: </label>
+                  <input type="text" name="email"/>
+                  <label>Password: </label>
+                  <input type="text" name="password"/>
+                  <div className='btn ion-text-center'>
+                    <IonButton 
+                      type='submit' 
+                      color="secondary" 
+                      className='ion-text-center'
+                      expand='block'
+                      ref={btnLogin}
+                    > Login
+                    </IonButton>   
+                  </div>     
+                </form>
+              </IonCol>
+            </IonRow>
+            <IonRow className="ion-text-center moveto-signup">
+              <IonCol className="ion-align-items-center">
+                <IonText>Haven't already account ? 
+                  <Link to={'signup'} style={linkStyle}>
+                    Sign Up now !
+                  </Link>
+                </IonText>
+              </IonCol>
+            </IonRow> 
+          </IonGrid>
+        </IonContent>
     </IonPage>
   );
 };
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,23 +1,62 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCol, IonContent, IonGrid, IonPage, IonRow, IonText, IonTitle} from '@ionic/react';
+import { useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const SignUp: React.FC = () => {
+  const [isSignUp, setIsSignUp] = useState(false);
+  const btnSignUp = useRef<HTMLIonButtonElement>(null);
+  
+  const linkStyle = {
+    margin: "1rem",
+    color: 'white'
+  };
+
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
+      <IonContent color='primary'>
+          <IonGrid className='content-login-signup'>
+            <IonRow className="ion-text-center">
+              <IonCol className="ion-align-items-center">
+                <IonTitle className='app-title'>Waddup</IonTitle> 
+              </IonCol>
+            </IonRow>
+            <IonRow className='ion-justify-content-center'>
+              <IonCol className="ion-align-items-center">
+                <form action="submit" className='ion-margin login_form '>
+                  <label>Name : </label>
+                  <input type="text" name="name"/>
+                  <label>User ID : </label>
+                  <input type="text" name="userId"/>
+                  <label>Email : </label>
+                  <input type="text" name="email"/>
+                  <label>Password : </label>
+                  <input type="text" name="password"/>
+                  <div className='btn ion-text-center'>
+                    <IonButton 
+                      type='submit' 
+                      color="secondary" 
+                      className='ion-text-center'
+                      expand='block'
+                      ref={btnSignUp}
+                    > Sign Up
+                    </IonButton>   
+                  </div>     
+                </form>
+              </IonCol>
+            </IonRow>
+            <IonRow className="ion-text-center moveto-signup">
+              <IonCol className="ion-align-items-center">
+                <IonText>Already have account ? 
+                  <Link to={'login'} style={linkStyle}>
+                    Sign In now !
+                  </Link>
+                </IonText>
+              </IonCol>
+            </IonRow> 
+          </IonGrid>
+        </IonContent>
     </IonPage>
   );
 };
