7c56bf1dcf10fb6e1c5d8edac92e9a3988913da2
umarhaqi9 <74255063+umarhaqi9@users.noreply.github.com>
2022-04-17 13:53:32 +0700

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,51 @@
-import { IonAvatar, IonButton, IonButtons, IonContent, IonFab, IonFabButton, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonMenuButton, IonPage, IonText, IonThumbnail, IonTitle, IonToolbar } from '@ionic/react';
+import { IonAvatar, IonBackButton, IonButton, IonButtons, IonCol, IonContent, IonFab, IonFabButton, IonGrid, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonList, IonMenuButton, IonModal, IonPage, IonRow, IonText, IonThumbnail, IonTitle, IonToast, IonToolbar } from '@ionic/react';
 import './Home.css';
 import { chatboxEllipsesOutline, searchOutline } from 'ionicons/icons';
+import { useState } from 'react';
 
-const Home: React.FC = () => (
+const Home: React.FC = () => {
+  const [ isCreating, setIsCreating ] = useState(false);
+  const [toastMessage, setToastMessage] = useState('');
+  
+  const startCreatingHandler = () => {
+    setIsCreating(true);
+  };
+
+  const createChatRoom = () => {
+    setIsCreating(false);
+    setToastMessage('Chat room has been created!');
+  }
+
+  return(
   <IonPage>
+    <IonModal isOpen={isCreating}>
+        <IonHeader>
+            <IonToolbar color='primary'>
+                <IonTitle>Create Chat Room</IonTitle>
+            </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
+            <IonItem color='secondary' lines="full" button onClick={createChatRoom}>
+              <IonThumbnail slot="start" className='ion-margin'>
+                <IonAvatar>
+                  <img src='https://media.discordapp.net/attachments/841587576464736266/946390659852546069/tasm3_confirmed_20220224_155923_0.jpg?width=338&height=338' />
+                </IonAvatar>
+              </IonThumbnail>
+              <IonLabel color='light' className='ion-margin'>
+                <IonText><strong>Nama Placeholder</strong></IonText><br />
+                <p>lorem ipsum dolor bae</p>
+                
+              </IonLabel>
+            </IonItem>
+          
+          <IonRow className='ion-text-center'>
+              <IonCol>
+                  <IonButton  color='secondary' onClick={() => setIsCreating(false)}>Cancel</IonButton>
+              </IonCol>
+          </IonRow>
+            
+        </IonContent>
+    </IonModal>
     <IonHeader>
       <IonToolbar color='primary'>
         <IonButtons slot='start'>
@@ -43,12 +85,17 @@ const Home: React.FC = () => (
           </IonLabel>
         </IonItem>
         <IonFab horizontal='end' vertical='bottom' slot='fixed'>
-            <IonFabButton color='primary' routerLink='/tabs/new'>
+            <IonFabButton color='primary' onClick={startCreatingHandler}>
                 <IonIcon icon={chatboxEllipsesOutline}/>
             </IonFabButton>
         </IonFab>
+        <IonToast isOpen={!!toastMessage}
+                    message={toastMessage}
+                    duration={3000}
+                    onDidDismiss={() => { setToastMessage('') }} />
     </IonContent>
   </IonPage>
-);
+  );
+}
 
 export default Home;
