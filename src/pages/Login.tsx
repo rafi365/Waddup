2bd3eaae0a44e4af205d80bd7b9544b1aaecb5f7
rafi365 <74255365+rafi365@users.noreply.github.com>
2022-05-10 21:54:14 +0700

    implement password reset option in login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,17 @@
-import { IonCol, IonContent, IonGrid, IonPage, IonRow, IonTitle, IonText, IonButton, IonInput, IonToast, useIonViewWillEnter } from '@ionic/react';
+import { IonCol, IonContent, IonGrid, IonPage, IonRow, IonTitle, IonText, IonButton, IonInput, IonToast, useIonViewWillEnter, IonHeader, IonModal, IonToolbar } from '@ionic/react';
 import { useRef, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import './Home.css';
 import { Controller, useForm } from 'react-hook-form'
-import { onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
+import { onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebaseConfig';
 
 const Login: React.FC = () => {
   const { register, handleSubmit } = useForm();
   const history = useHistory();
   const [toastMessage, setToastMessage] = useState('');
+  const [isReset, setIsReset] = useState(false);
+  const resetemailref = useRef<HTMLIonInputElement>(null);
   // onAuthStateChanged(auth, (user) => {
   //   if (user) {
   //     // User is signed in, see docs for a list of available properties
@@ -48,6 +50,25 @@ const Login: React.FC = () => {
     }
   }
 
+  const doresetpass = (e: any) => {
+    e.preventDefault();
+    const email: string = resetemailref.current?.value ? resetemailref.current?.value.toString() : ""
+    console.log(email);
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        // Password reset email sent!
+        // ..
+        setToastMessage('Password Reset Email Sent!')
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+        setToastMessage(errorMessage)
+      });
+
+  }
+
   const linkStyle = {
     margin: "1rem",
     color: 'white'
@@ -81,6 +102,15 @@ const Login: React.FC = () => {
               </form>
             </IonCol>
           </IonRow>
+          <IonRow className="ion-text-center">
+            <IonCol className="ion-align-items-center">
+              <IonText>Forgot your password?
+                <a onClick={() => setIsReset(true)} style={linkStyle}>
+                  Click Here!
+                </a>
+              </IonText>
+            </IonCol>
+          </IonRow>
           <IonRow className="ion-text-center moveto-signup">
             <IonCol className="ion-align-items-center">
               <IonText>Don't have an account ?
@@ -96,6 +126,52 @@ const Login: React.FC = () => {
         message={toastMessage}
         duration={2000}
         onDidDismiss={() => { setToastMessage('') }} />
+
+
+
+      <IonModal isOpen={isReset} backdropDismiss={false}>
+        <IonHeader>
+          <IonToolbar color='primary'>
+            <IonTitle>Password Reset</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent color='primary'>
+          <IonGrid className='content-login-signup'>
+            <IonRow className="ion-text-center">
+              <IonCol className="ion-align-items-center">
+                <IonTitle className='app-title'>Waddup</IonTitle>
+              </IonCol>
+            </IonRow>
+            <IonRow className='ion-justify-content-center'>
+              <IonCol className="ion-align-items-center">
+                <form onSubmit={doresetpass} className='ion-margin login_form '>
+                  <label>Email: </label>
+                  <IonInput type="email" ref={resetemailref} required className='inputcss' />
+                  <div className='btn ion-text-center'>
+                    <IonButton
+                      type='submit'
+                      color="secondary"
+                      className='ion-text-center'
+                      expand='block'
+                    > Reset Password
+                    </IonButton>
+                    <IonButton
+                      type='button'
+                      color="secondary"
+                      className='ion-text-center'
+                      expand='block'
+                      onClick={() => setIsReset(false)}
+                    > Go Back
+                    </IonButton>
+                  </div>
+                </form>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonContent>
+      </IonModal>
+
+      
     </IonPage>
   );
 };
