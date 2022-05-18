83d0d7fcd282b77bf7c16985a91644fd00d1dcbe
rafi365 <74255365+rafi365@users.noreply.github.com>
2022-05-18 22:39:27 +0700

    change android package name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.umn.waddup',
   appName: 'Waddup',
   webDir: 'build',
   bundledWebRuntime: false
